d2e9b6a0ac73febac5e94a8d0cc1376d72ddbec7
Annie Huang <annie.hn@gmail.com>
2022-02-05 16:41:30 +1100

    Dispatch incremented action when button is click through useAppDispatch()

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented } from './features/counter/counter-slice';
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  function handleClick() {
+    dispatch(incremented());
+  }
 
   return (
     <div className='App'>
-      <button>count is: {count}</button>
+      <button onClick={handleClick}>count is: {count}</button>
     </div>
   );
 }
